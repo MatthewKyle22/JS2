5422d600bbb3f934339d143d92566f7f957e9556
Matt <matthewky22@gmail.com>
2016-04-08 17:39:46 +0000

    Second JS2 commit

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 var question = prompt("Enter a word!");
 
-console.log(alert('Your word was ' + question + '!' ));
-
 var length = question.length;
-console.log(length);
+// console.log(alert('There are ' + length + ' characters in the word'));
+var char = question.charAt(2);
+var lower = question.toLowerCase('');
+var upper = question.toUpperCase('');
+var sentence = "The " + question + ' is a great magazine!';
+var sub = question.substring(1, 4);
+
+console.log(alert('Your word was ' + question + '! \n' +
+    'There are ' + length + ' characters in the word \n' +
+    "The third character is '" +  char + "'.\n" + 
+    'Lowercase: ' + lower +
+    '\nUppercase: ' + upper +
+    '\nExample: ' + sentence +
+    '\nSubword: ' + sub));
